628160cb1314dfad4d3ff4d671306052d1c06d3f
« jokecki » <jean.okecki@gmail.com>
2025-03-08 21:12:53 +0100

    Initial commit: Minimal Streamlit app

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Hello from Streamlit")
+st.write("Ceci est un test pour valider le setup Streamlit.")
+
+if st.button("Clique moi"):
+    st.write("Bouton cliqué !")
+
